dacc6132cf253f56fec65cc2f2fe60d329e8cbbc
EmmanuelDiomande <emmanuel.diomande@outlook.com>
2024-02-11 16:35:38 +0000

    Update V5

--- _nuxt/index.i1E9egVM.js
+++
@@ -1,1 +0,0 @@
-import{f as o}from"./entry.4cX2zxFU.js";const e={__name:"index",setup(a){return o(()=>{window.location.assign("https://api-dooya.dooya.ci/dashboard")}),(n,t)=>null}};export{e as default};
